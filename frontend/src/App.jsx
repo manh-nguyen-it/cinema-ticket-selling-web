45f6281d87a475b45997c7103e02616d04ce137a
manh05 <tanmanhtn2005@gmail.com>
2025-11-15 08:26:54 +0700

    sua phan cau hinh route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,6 @@ import './style.css'
 function App() {
     return (
         <BrowserRouter>
-            {/* Linking */}
-            <div className="site-map">
-                <h3>Site Map</h3>
-                <Link to="/header">Header</Link>
-                
-
-            </div>
-
-            {/* Route Configuration */}
             <Routes>
                 <Route path="/header" element={<Header />}/>
             </Routes>    
